7f59d7ce4542b9438ee4449840088eeea586ff44
DanielP <dpalmer9@uwo.ca>
2024-01-15 15:26:24 -0500

    Patched naming error

--- Protocol/iCPT2GStim1/Task/Menu.py
+++ Protocol/iCPT2GStim1/Task/Menu.py
@@ -4,6 +4,6 @@ class ConfigureScreen(MenuBase):
     def __init__(self, **kwargs):
         super(ConfigureScreen, self).__init__(**kwargs)
 
-        self.protocol = 'iCPT2GStim2'
+        self.protocol = 'iCPT2GStim1'
 
         self.menu_constructor(self.protocol)
