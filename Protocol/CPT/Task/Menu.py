f7e704848f8f8010e1ee7913f49ad8ece22e1577
dpalmer9 <dpalmer9@uwo.ca>
2025-10-22 11:26:07 -0400

    Template code for image selector added

--- Protocol/CPT/Task/Menu.py
+++ Protocol/CPT/Task/Menu.py
@@ -1,4 +1,4 @@
-from Classes.Menu import MenuBase
+from Classes.Menu import MenuBase, ImageSetDropdown
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.dropdown import DropDown
@@ -12,4 +12,13 @@ class ConfigureScreen(MenuBase):
         self.protocol_title_label.text = self.protocol_name
         self.name = self.protocol + '_configscreen'
 
+        self.image_select_label = Label(text='Image Set:')
+        self.image_selector = ImageSetDropdown([
+            {'label': 'Image Set 1',
+            'images': ['Protocol/CPT/Image/Fribbles/Fb/Fb2_1111.png', 'Protocol/CPT/Image/Fribbles/Fb/Fb2_1112.png'],
+            'value': 'set1'}
+            ])
         self.menu_constructor(self.protocol)
+        self.setting_gridlayout.rows += 1
+        self.setting_gridlayout.add_widget(self.image_select_label)
+        self.setting_gridlayout.add_widget(self.image_selector)
