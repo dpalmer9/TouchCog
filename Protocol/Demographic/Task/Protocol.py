df3a40c1a4d3337bfb433b2d50ccd1814d769868
dpalmer9 <dpalmer9@uwo.ca>
2025-10-31 10:56:42 -0400

    Initial Survey Implementation

---
+++ Protocol/Demographic/Task/Protocol.py
@@ -0,0 +1,33 @@
+# Imports
+from Classes.Survey import SurveyBase
+from kivy.app import App
+import pathlib
+import json
+
+
+# Survey Protocol Class
+
+class SurveyProtocol(SurveyBase):
+    """
+    A demographic survey protocol for collecting participant information.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = 'Demographic'
+        self.app = App.get_running_app()
+        self.survey_name = "Demographic Survey"
+        self.survey_description = "A survey to collect demographic information from participants."
+        self.end_survey_text = "Thank you for completing the demographic survey."
+
+
+        self.survey_json = None
+        self.language = self.app.language
+        self.survey_path = pathlib.Path('Protocol', self.name, 'Language', self.language, 'survey.json')
+
+        with open(self.survey_path) as f:
+            self.survey_json = json.load(f)
+
+        self.load_survey(self.survey_json)
+
+        self.start_screen()
