48900a27b78c52835029c10cdb2c491c7cade25b
Filip Kosel <109816625+FilipKosel@users.noreply.github.com>
2025-11-13 10:55:43 -0500

    PRL bug fixes/minor updates
    Video
    - Adjusted video position based on text button size
    - Added fit_mode = 'contain' to resize video to fit screen without changing aspect ratio
    - Added restart video function
    - Added boolean variable to check for first play of tutorial video to trigger addition of restart/continue buttons
    
    Text Buttons
    - Adjusted size/position slightly
    - Changed to sentence/title case
    
    Feedback
    - Added "points_awarded" feedback to feedback file
    - Added "points_awarded" feedback import to feedback_dict in protocol
    - Training stage uses "correct" feedback, probe uses "points_awarded" feedback

--- Protocol/PRL/Task/Protocol.py
+++ Protocol/PRL/Task/Protocol.py
@@ -269,11 +269,24 @@ class ProtocolScreen(ProtocolBase):
 
 		self.lang_folder_path = pathlib.Path('Protocol', self.protocol_name, 'Language', self.language)
 
+		feedback_lang_path = self.lang_folder_path / 'Feedback.ini'
+		feedback_lang_config = configparser.ConfigParser(allow_no_value=True)
+		feedback_lang_config.read(feedback_lang_path, encoding='utf-8')
+
+		points_awarded_feedback_string = '[color=%s]' \
+			% feedback_lang_config['Stimulus']['points_awarded_colour'] \
+			+ feedback_lang_config['Stimulus']['points_awarded'] \
+			+ '[/color]'
+		
+		self.feedback_dict['points_awarded'] = points_awarded_feedback_string
+
 		if (self.lang_folder_path / 'Tutorial_Video').is_dir():
 			self.tutorial_video_path = str(list((self.lang_folder_path / 'Tutorial_Video').glob('*.mp4'))[0])
-			self.tutorial_video = Video(source = self.tutorial_video_path)
-			self.tutorial_video.pos_hint = {'center_x': 0.5, 'center_y': 0.6}
-			self.tutorial_video.size_hint = (1, 1)
+			self.tutorial_video = Video(
+				source = self.tutorial_video_path
+				, pos_hint = {'center_x': 0.5, 'center_y': 0.5 + self.text_button_size[1]}
+				, fit_mode = 'contain'
+				)
 
 
 		# Define Widgets - Instructions
@@ -322,17 +335,17 @@ class ProtocolScreen(ProtocolBase):
 
 		self.protocol_floatlayout.clear_widgets()
 
-		self.tutorial_restart = Button(text='RESTART VIDEO', font_size='48sp')
-		self.tutorial_restart.size_hint = self.text_button_size
-		self.tutorial_restart.pos_hint = self.text_button_pos_LL
-		self.tutorial_restart.bind(on_press=self.start_tutorial_video)
+		self.tutorial_restart_button = Button(text='Restart Video', font_size='48sp')
+		self.tutorial_restart_button.size_hint = self.text_button_size
+		self.tutorial_restart_button.pos_hint = self.text_button_pos_LL
+		self.tutorial_restart_button.bind(on_press=self.tutorial_restart)
 		
-		self.tutorial_start_button = Button(text='START TASK', font_size='48sp')
+		self.tutorial_start_button = Button(text='Start Task', font_size='48sp')
 		self.tutorial_start_button.size_hint = self.text_button_size
 		self.tutorial_start_button.pos_hint = self.text_button_pos_LR
-		self.tutorial_start_button.bind(on_press=self.start_protocol_from_tutorial)
+		self.tutorial_start_button.bind(on_press=self.stop_tutorial_video)
 		
-		self.tutorial_video_button = Button(text='TAP THE SCREEN\nTO START VIDEO', font_size='48sp', halign='center', valign='center')
+		self.tutorial_video_button = Button(text='Tap the screen\nto start video', font_size='48sp', halign='center', valign='center')
 		self.tutorial_video_button.background_color = 'black'
 		self.tutorial_video_button.size_hint = (1, 1)
 		self.tutorial_video_button.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
@@ -344,33 +357,49 @@ class ProtocolScreen(ProtocolBase):
 		self.protocol_floatlayout.add_widget(self.tutorial_video_button)
 
 		self.tutorial_video.state = 'stop'
+
+		self.tutorial_video_first_play = True
+
+		return
 	
 
 
 	def start_tutorial_video(self, *args):
 
 		self.tutorial_video.state = 'play'
-		self.protocol_floatlayout.remove_widget(self.tutorial_video_button)
-		
-		Clock.schedule_once(self.present_tutorial_video_start_button, self.tutorial_video_duration)
-		
-		self.protocol_floatlayout.add_object_event('Display', 'Video', 'Section', 'Instructions')
+
+		if self.tutorial_video_first_play:
+			self.tutorial_video_first_play = False
+			self.protocol_floatlayout.remove_widget(self.tutorial_video_button)
+			Clock.schedule_once(self.present_tutorial_video_start_button, self.tutorial_video_duration)
+			self.protocol_floatlayout.add_object_event('Display', 'Video', 'Section', 'Instructions')
+
+		return
 
 
 
 	def present_tutorial_video_start_button(self, *args):
 
 		self.protocol_floatlayout.add_widget(self.tutorial_start_button)
-		self.protocol_floatlayout.add_widget(self.tutorial_restart)
+		self.protocol_floatlayout.add_widget(self.tutorial_restart_button)
 				
 		self.protocol_floatlayout.add_object_event('Display', 'Button', 'Section', 'Instructions', 'Section Start')
 		self.protocol_floatlayout.add_object_event('Display', 'Button', 'Section', 'Instructions', 'Video Restart')
+		return
+	
+	
+	def stop_tutorial_video(self, *args):
+		self.tutorial_video.state = 'stop'
+		self.protocol_floatlayout.add_object_event('Remove', 'Video', 'Section', 'Instructions')
+		self.start_protocol_from_tutorial()
+
+	def tutorial_restart(self, *args):
+		self.tutorial_video.state = 'stop'
+		self.start_tutorial_video()
 	
 	
 	
 	def start_protocol_from_tutorial(self, *args):
-		self.tutorial_video.state = 'stop'
-
 		self.protocol_floatlayout.clear_widgets()
 
 		self.protocol_floatlayout.add_object_event('Remove', 'Video', 'Section', 'Instructions')
@@ -525,10 +554,10 @@ class ProtocolScreen(ProtocolBase):
 			self.protocol_floatlayout.add_stage_event('Choice Rewarded')
 
 			if self.current_stage == 'Training':
-				self.feedback_label.text = '[color=00C000]CORRECT[/color]'
+				self.feedback_label.text = self.feedback_dict['correct']
 
 			else:
-				self.feedback_label.text = self.feedback_dict['correct']
+				self.feedback_label.text = self.feedback_dict['points_awarded']
 				self.point_counter += 10
 
 				self.protocol_floatlayout.add_stage_event('Points Collected')
