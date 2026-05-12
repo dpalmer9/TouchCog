a3fce8ee00bc0e85d78617229cde4a110208d9d5
NeuralFire <daniel.palmer321@gmail.com>
2026-05-12 08:24:36 -0400

    Updated CJB Video Path

--- Protocol/CPT/Task/Protocol.py
+++ Protocol/CPT/Task/Protocol.py
@@ -812,7 +812,7 @@ class ProtocolScreen(ProtocolBase):
 			self.sim_scale_video_path = self.lang_folder_path / 'Tutorial_Video' / 'CPT-General-2025-09-18.mp4'
 			self.stimdur_video_path = self.lang_folder_path / 'Tutorial_Video' / 'CPT-StimDur-2025-09-18.mp4'
 			self.tar_prob_video_path = self.lang_folder_path / 'Tutorial_Video' / 'CPT-General-2025-09-18.mp4'
-			self.cjb_train_video_path = self.lang_folder_path / 'Tutorial_Video' / 'CJB-Main-2026-05-05.mp4'
+			self.cjb_train_video_path = self.lang_folder_path / 'Tutorial_Video' / 'CJB-Main-2026-05-11.mp4'
 
 			if self.cjb_task:
 				start_tutorial_path = self.cjb_train_video_path
